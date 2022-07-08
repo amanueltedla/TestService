d8fcbbaaffc7debf2dfb1ba6d5d23b30b372c0a7
Amanuel Tedla <Amanuel.Tedla@azenia.com>
2022-07-08 14:22:19 +0300

    Change range to 4

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,10 +27,10 @@ namespace TestService.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 4).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-20, 25),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
