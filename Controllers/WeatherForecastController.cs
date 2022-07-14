bb9c4269f79b0076e08946237097ef14aa711266
bereket9901 <89933744+bereket9901@users.noreply.github.com>
2022-07-14 15:37:59 +0300

    Bereket

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -41,10 +41,10 @@ namespace TestService.Controllers
         public IEnumerable<WeatherForecast> NewAPI()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 7).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(-10, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
